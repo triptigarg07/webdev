75a41d2f21e78de12671bfac5ce1b8e7df56b8a4
Tripti Garg <tripti200407garg@gmail.com>
2023-06-15 16:36:51 +0530

    added some files

---
+++ tut1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a[10];
+    for(i=0;i<9;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    for(i=0;i<9;i++)
+    {
+        printf("%d",a[i]);
+    }
+}
